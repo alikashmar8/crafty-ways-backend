e8906fd16cc31205bbc74a019b7f74bfb3953eaf
alikashmar8 <alikashmar8@gmail.com>
2021-12-07 20:18:58 +0200

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,32 @@ import { ImagesModule } from './images/images.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
+    TypeOrmModule.forRoot(
+    //   {
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'crafty_ways_db',
+    //   synchronize: true,
+    //   logging: false,
+    //   entities: ['dist/**/*.entity{.ts,.js}'],
+    //   migrations: ['src/migrations/**/*.ts'],
+    // }
+      {
+      type: 'mysql',
+      host: 'craftywaysdb.cbmemndkpzaq.eu-west-1.rds.amazonaws.com',
       port: 3306,
       username: 'root',
-      password: 'root',
-      database: 'crafty_ways_db',
+      password: 'craftywaysdb',
+      database: 'craftywaysdb',
       synchronize: true,
       logging: false,
       entities: ['dist/**/*.entity{.ts,.js}'],
       migrations: ['src/migrations/**/*.ts'],
-    }),
+    }
+    ),
     UsersModule,
     AuthModule,
     CategoriesModule,
