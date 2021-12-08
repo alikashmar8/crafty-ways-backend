740f5b60128a1dc83f6af93a7a58048adb4bc964
alikashmar8 <alikashmar8@gmail.com>
2021-12-08 20:59:13 +0200

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use('/uploads', express.static('uploads'));
-  app.enableCors()
   const config = new DocumentBuilder()
     .setTitle('Crafty Ways')
     .setDescription('API documentation for crafty ways')
